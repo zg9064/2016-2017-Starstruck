60cb090811192fa3659daefdd38019a2af2f0da4
Cipher032 <solowingpixycipher@gmail.com>
2016-12-01 22:50:53 -0800

    STOP ADDING PID

--- Kilo v2 (Claw)/main.c
+++ Kilo v2 (Claw)/main.c
@@ -22,23 +22,11 @@
 #define LOOP_SPEED              25
 #define MAX_POWER              127
 #define MIN_POWER             -127
-//#define PID_SENSOR_INDEX    myEncoder
-//#define PID_SENSOR_SCALE    1
 
-//#define PID_MOTOR_INDEX     myMotor
-//#define PID_MOTOR_SCALE     -1
-
-//#define PID_INTEGRAL_LIMIT  50
 
 #define SAFETY_DELAY				200
 
-/*float  pid_Kp = 2.0;
-float  pid_Ki = 0.04;
-float  pid_Kd = 0.0;
 
-static int   pidRunning = 1;
-static float pidRequestedValue;
-*/
 // final motor drive
 long            motor_driveR;            /// RIGHT
 long            motor_driveL;            /// LEFT
@@ -192,85 +180,6 @@ allStop()
 	setLift( 1000, 0 );
 	setClaw( 1000, 0 );
 }
-
-
-//task
-/*#warning "PID Controller"
-task pidController()
-{
-	/*
-	float  pidSensorCurrentValue;
-
-	float  pidError;
-	float  pidLastError;
-	float  pidIntegral;
-	float  pidDerivative;
-	float  pidDrive;
-
-	// If we are using an encoder then clear it
-	if( SensorType[ PID_SENSOR_INDEX ] == sensorQuadEncoder )
-	SensorValue[ PID_SENSOR_INDEX ] = 0;
-
-	// Init the variables - thanks Glenn :)
-	pidLastError  = 0;
-	pidIntegral   = 0;
-
-	while( true )
-	{
-	// Is PID control active ?
-	if( pidRunning )
-	{
-	// Read the sensor value and scale
-	pidSensorCurrentValue = SensorValue[ PID_SENSOR_INDEX ] * PID_SENSOR_SCALE;
-
-	// calculate error
-	pidError = pidSensorCurrentValue - pidRequestedValue;
-
-	// integral - if Ki is not 0
-	if( pid_Ki != 0 )
-	{
-	// If we are inside controlable window then integrate the error
-	if( abs(pidError) < PID_INTEGRAL_LIMIT )
-	pidIntegral = pidIntegral + pidError;
-	else
-	pidIntegral = 0;
-	}
-	else
-	pidIntegral = 0;
-
-	// calculate the derivative
-	pidDerivative = pidError - pidLastError;
-	pidLastError  = pidError;
-
-	// calculate drive
-	pidDrive = (pid_Kp * pidError) + (pid_Ki * pidIntegral) + (pid_Kd * pidDerivative);
-
-	// limit drive
-	if( pidDrive > PID_DRIVE_MAX )
-	pidDrive = PID_DRIVE_MAX;
-	if( pidDrive < PID_DRIVE_MIN )
-	pidDrive = PID_DRIVE_MIN;
-
-	// send to motor
-	motor[ PID_MOTOR_INDEX ] = pidDrive * PID_MOTOR_SCALE;
-	}
-	else
-	{
-	// clear all
-	pidError      = 0;
-	pidLastError  = 0;
-	pidIntegral   = 0;
-	pidDerivative = 0;
-	motor[ PID_MOTOR_INDEX ] = 0;
-	}
-
-	// Run at 50Hz
-	wait1Msec( 25 );
-	}
-	*/
-}
-*/
-
 
 #warning "pre-auton"
 void pre_auton()
