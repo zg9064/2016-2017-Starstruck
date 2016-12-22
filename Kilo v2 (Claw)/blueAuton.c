7d3999da1143069af09e550c4019879c91df878d
Abner B <jorgeabner2000@gmail.com>
2016-12-22 08:56:47 -0800

    Segmentation

---
+++ Kilo v2 (Claw)/blueAuton.c
@@ -0,0 +1,12 @@
+/*
+Disregard the preloaded star and drive forward while lifting the lift on level with the fence and extending the claw; should knock the three stars into the near zone
+The robot will lower itself and turn to face the middle cube.
+The robot will grab the cube that is sitting at position #3 and reverse dump the cube in the (left-ish or right-ish) middle of the fence; should be one cube and two stars that go into the near zone.
+The robot will turn around and start heading to the 3 stars in the middle, grab a minimum of 2 stars; should be two stars that go into the near zone.
+The robot will head to position #5 and dump the two stars it holds as well as knock the two stars that are on the (left-ish or right-ish) part of the fence.
+*/
+
+main()
+{
+
+}
---
+++ Kilo v2 (Claw)/universalAuton.c
@@ -0,0 +1,9 @@
+/*
+This autonomous program should work for both blue and red alliance
+It opens the robot's claw, lifts lift, drives forward, and knocks one or two stars
+*/
+
+main()
+{
+  
+}
